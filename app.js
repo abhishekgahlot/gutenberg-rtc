79fa6008a5ad06efca06552325250085fd84ff90
Abhishek Gahlot <me@abhishek.it>
2017-06-28 23:48:19 +0530

    added more packages

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
-var Peer = require('simple-peer')
-var p = new Peer({ initiator: location.hash === '#1', trickle: false })
-window.p = p;
-p.on('error', function (err) { console.log('error', err) })
+'use strict';
 
-p.on('signal', function (data) {
-  console.log('SIGNAL', JSON.stringify(data))
-  document.querySelector('#outgoing').textContent = JSON.stringify(data)
-})
+const Peer = require('simple-peer');
+const uuidv1 = require('uuid/v1');
 
-document.querySelector('form').addEventListener('submit', function (ev) {
-  ev.preventDefault()
-  p.signal(JSON.parse(document.querySelector('#incoming').value))
-})
 
-p.on('connect', function () {
-  console.log('CONNECT')
-  p.send('whatever' + Math.random())
-})
+class GRTC {
+	constructor() {
+		this.uuid = uuidv1();
+	}
+}
 
-p.on('data', function (data) {
-  console.log('data: ' + data)
-})
+module.exports = GRTC;
