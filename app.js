3d60a285bec4b31e5ad78ea1b59a0e4adb923899
Abhishek Gahlot <me@abhishek.it>
2017-06-29 02:11:54 +0530

    changes to app

--- app.js
+++ app.js
@@ -5,9 +5,40 @@ const uuidv1 = require('uuid/v1');
 
 
 class GRTC {
-	constructor() {
-		this.uuid = uuidv1();
-	}
+	constructor(uuid, joinee) {
+        this.peer = null;
+        this.signal = null;
+        this.joinee = joinee;
+        this.start();
+    }
+
+    static uuid() {
+        return uuidv1()
+    }
+
+    peerHandler() {
+        this.peer.on('signal', (signal) => {
+            this.signal = signal;
+        });
+    }
+
+    start() {
+        let peerInterface = new Peer({ 
+            initiator: this.joinee === true,
+            trickle: false
+        });
+        this.peer = peerInterface;
+        this.peerHandler();
+    }
+
+    send(data) {
+        this.peer.send(data);
+    }
 }
 
-module.exports = GRTC;
+
+if (Peer.WEBRTC_SUPPORT) {
+    global.GRTC = GRTC;
+} else {
+    global.GRTC = null;
+}
