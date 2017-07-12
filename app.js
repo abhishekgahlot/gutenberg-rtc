7d9c3a22ec94f729f3e12743368b710aaf189883
Abhishek Gahlot <me@abhishek.it>
2017-07-12 06:25:58 +0530

    added function for random color generation

--- app.js
+++ app.js
@@ -167,6 +167,18 @@ class GRTC extends EventEmitter {
     }
 
     /**
+     * Random color generate for peer.
+     */
+    static randomColor() {
+        let letters = '0123456789ABCDEF';
+        let color = '#';
+        for ( let i = 0; i < 6; i++ ) {
+            color += letters[ Math.floor(Math.random() * 16 ) ];
+        }
+        return color;
+    }
+
+    /**
      * Generates uuid which is used for url unique hash.
      */
     static uuid() {
